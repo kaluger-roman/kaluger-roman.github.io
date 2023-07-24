5a8d8cc54572b5dab354aad8eb7a2295038e6f84
Roman Kaluger <r.kaluger@sftpro.ru>
2023-07-24 22:25:39 +0300

    try

--- tunnel-hosts.js
+++ tunnel-hosts.js
@@ -1,7 +1,7 @@
 export const tunnelsHosts = {
   tunnels: {
-    "http://localhost:3024": "https://e4f3-46-246-94-225.ngrok-free.app",
-    "http://localhost:3025": "https://d742-85-174-195-64.ngrok-free.app",
+    "http://localhost:3024": "https://25e4-46-246-94-225.ngrok-free.app",
+    "http://localhost:3025": "https://b6e5-46-246-94-225.ngrok-free.app",
   },
   clientHost: "http://localhost:3024",
 };
