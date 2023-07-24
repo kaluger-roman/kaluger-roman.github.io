fb34ae53b8d5a1bb2b148fed88d263045783da54
Roman Kaluger <r.kaluger@sftpro.ru>
2023-07-24 21:10:19 +0300

    try

---
+++ setup-connetion.js
@@ -0,0 +1,25 @@
+const tunnelsHosts = require("./tunnel-hosts.json");
+
+const fetchHost = async () => {
+  const res = await fetch(tunnelsHosts.tunnels[tunnelsHosts.clientHost], {
+    method: "GET",
+    headers: {
+      "ngrok-skip-browser-warning": true,
+    },
+  });
+  const blob = await res.blob();
+  const urlObject = URL.createObjectURL(blob);
+
+  const frame = document.querySelector("iframe");
+
+  frame.setAttribute("src", urlObject);
+
+  frame.onload = () => {
+    frame?.contentWindow.postMessage(
+      { type: "setup-tunnels", tunnels: tunnelsHosts.tunnels },
+      "*"
+    );
+  };
+};
+
+fetchHost();
