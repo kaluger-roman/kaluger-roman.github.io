165058d98186a998fd247ef50c6960cb9af548c8
Roman Kaluger <r.kaluger@sftpro.ru>
2023-07-25 23:10:24 +0300

    try

--- tunnel-hosts.js
+++ tunnel-hosts.js
@@ -1,7 +1,13 @@
 export const tunnelsHosts = {
   tunnels: {
-    "http://localhost:3024": "https://25e4-46-246-94-225.ngrok-free.app",
-    "http://localhost:3025": "https://b6e5-46-246-94-225.ngrok-free.app",
+    "http://localhost:3021":
+      "https://283a-85-174-195-64.ngrok-free.app/authorization-client",
+    "http://localhost:3022":
+      "https://283a-85-174-195-64.ngrok-free.app/authorization-server",
+    "http://localhost:3024":
+      "https://283a-85-174-195-64.ngrok-free.app/sound-english-client",
+    "http://localhost:3025":
+      "https://283a-85-174-195-64.ngrok-free.app/sound-english-server",
   },
   clientHost: "http://localhost:3024",
 };
