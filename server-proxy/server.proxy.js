0ab3632b89d4a08d742829800739b81d2211dc62
Roman Kaluger <r.kaluger@sftpro.ru>
2023-12-27 23:19:19 +0300

    cert and proxy

--- server-proxy/server.proxy.js
+++ server-proxy/server.proxy.js
@@ -1,7 +1,7 @@
 const proxy = require("express-http-proxy");
 const app = require("express")();
 const https = require("https");
-
+const tls = require("tls");
 const env = require("dotenv").config();
 
 const soundEnglishEnv = require("dotenv").config({
@@ -12,9 +12,40 @@ const authEnv = require("dotenv").config({
   path: "../deploy/environment/authorization-client.env",
 });
 
-const httpsOptions = {
-  cert: fs.readFileSync(env.SOUND_ENGLISH_CERT_PATH),
-  key: fs.readFileSync(env.SOUND_ENGLISH_CERT_KEY_PATH),
+const certs = {
+  [env.SOUND_ENGLISH_DOMAIN]: {
+    key: env.SOUND_ENGLISH_CERT_KEY_PATH,
+    cert: env.SOUND_ENGLISH_CERT_PATH,
+  },
+  [env.SOUND_ENGLISH_AUTH_DOMAIN]: {
+    key: env.SOUND_ENGLISH_AUTH_CERT_KEY_PATH,
+    cert: env.SOUND_ENGLISH_AUTH_CERT_PATH,
+  },
+};
+
+const getSecureContexts = (certs) => {
+  const certsToReturn = {};
+  for (const serverName of Object.keys(certs)) {
+    certsToReturn[serverName] = tls.createSecureContext({
+      key: fs.readFileSync(certs[serverName].key),
+      cert: fs.readFileSync(certs[serverName].cert),
+    });
+  }
+  return certsToReturn;
+};
+
+const secureContexts = getSecureContexts(certs);
+
+const options = {
+  SNICallback: (servername, cb) => {
+    const ctx = secureContexts[servername];
+
+    if (cb) {
+      cb(null, ctx);
+    } else {
+      return ctx;
+    }
+  },
 };
 
 app.use(
@@ -32,3 +63,5 @@ app.use(
 );
 
 https.createServer(options, app).listen(443);
+
+app.listen(80);
