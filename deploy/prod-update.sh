426bff4f2323122f8738dcbf5059f6869c07e305
Roman Kaluger <r.kaluger@sftpro.ru>
2024-01-05 14:37:29 +0600

    new translator

--- deploy/prod-update.sh
+++ deploy/prod-update.sh
@@ -1,3 +1,4 @@
+git pull
 docker stop $(docker ps -a --filter network="kaluger-projects-prod_kaluger-projects-prod" -q)
 docker rm $(docker ps -a --filter network="kaluger-projects-prod_kaluger-projects-prod" -q)
 docker system prune -a -f --filter label=AUTO_UPDATE=true
